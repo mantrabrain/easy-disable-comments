0eec3d17363c0a6839d07732a9c43b5a76dad953
mantrabrain <mantrabrain@gmail.com>
2019-03-03 15:16:22 +0545

    comment deleted added

--- includes/settings.php
+++ includes/settings.php
@@ -14,8 +14,10 @@ foreach( array_keys( $types ) as $type ) {
 }
 
 if ( isset( $_POST['submit'] ) ) {
+
 	check_admin_referer( 'disable-comments-admin' );
-	$this->options['remove_everywhere'] = ( $_POST['mode'] == 'remove_everywhere' );
+
+	$this->options['remove_everywhere'] = ( isset($_POST['mode']) &&  $_POST['mode'] == 'remove_everywhere' );
 
 	if( $this->options['remove_everywhere'] )
 		$disabled_post_types = array_keys( $types );
@@ -36,6 +38,7 @@ if ( isset( $_POST['submit'] ) ) {
 	$cache_message = WP_CACHE ? ' <strong>' . __( 'If a caching/performance plugin is active, please invalidate its cache to ensure that changes are reflected immediately.' ) . '</strong>' : '';
 	echo '<div id="message" class="updated"><p>' . __( 'Options updated. Changes to the Admin Menu and Admin Bar will not appear until you leave or reload this page.', 'easy-disable-comments' ) . $cache_message . '</p></div>';
 }
+
 ?>
 <style> .indent {padding-left: 2em} </style>
 <div class="wrap">
@@ -48,13 +51,16 @@ if( WP_CACHE )
 ?>
 <form action="" method="post" id="disable-comments">
 <ul>
-<li><label for="remove_everywhere"><input type="radio" id="remove_everywhere" name="mode" value="remove_everywhere" <?php checked( $this->options['remove_everywhere'] );?> /> <strong><?php _e( 'Everywhere', 'easy-disable-comments') ?></strong>: <?php _e( 'Disable all comment-related controls and settings in WordPress.', 'easy-disable-comments') ?></label>
+<li><label for="remove_everywhere"><input type="radio" id="remove_everywhere" name="mode" value="remove_everywhere" <?php checked( isset($this->options['remove_everywhere']) ? $this->options['remove_everywhere']: 0 );?> /> <strong><?php _e( 'Everywhere', 'easy-disable-comments') ?></strong>: <?php _e( 'Disable all comment-related controls and settings in WordPress.', 'easy-disable-comments') ?></label>
 	<p class="indent"><?php printf( __( '%s: This option is global and will affect your entire site. Use it only if you want to disable comments <em>everywhere</em>.', 'easy-disable-comments' ), '<strong style="color: #900">' . __('Warning', 'easy-disable-comments') . '</strong>'); ?></p>
 </li>
-<li><label for="selected_types"><input type="radio" id="selected_types" name="mode" value="selected_types" <?php checked( ! $this->options['remove_everywhere'] );?> /> <strong><?php _e( 'On certain post types', 'easy-disable-comments') ?></strong>:</label>
+<li><label for="selected_types"><input type="radio" id="selected_types" name="mode" value="selected_types" <?php checked( ! isset($this->options['remove_everywhere']) || empty($this->options['remove_everywhere']) );?> /> <strong><?php _e( 'On certain post types', 'easy-disable-comments') ?></strong>:</label>
 	<p></p>
 	<ul class="indent" id="listoftypes">
-		<?php foreach( $types as $k => $v ) echo "<li><label for='post-type-$k'><input type='checkbox' name='disabled_types[]' value='$k' ". checked( in_array( $k, $this->options['disabled_post_types'] ), true, false ) ." id='post-type-$k'> {$v->labels->name}</label></li>";?>
+
+		<?php
+        $this->options['disabled_post_types'] = isset($this->options['disabled_post_types']) ? $this->options['disabled_post_types']: array();
+        foreach( $types as $k => $v ) echo "<li><label for='post-type-$k'><input type='checkbox' name='disabled_types[]' value='$k' ". checked( in_array( $k, $this->options['disabled_post_types'] ), true, false ) ." id='post-type-$k'> {$v->labels->name}</label></li>";?>
 	</ul>
 	<?php if( $this->networkactive ) :?>
 	<p class="indent" id="extratypes"><?php _e( 'Only the built-in post types appear above. If you want to disable comments on other custom post types on the entire network, you can supply a comma-separated list of post types below (use the slug that identifies the post type).', 'easy-disable-comments' ); ?>
